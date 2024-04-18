536df3acb881cf00f2231c3e3afdbb46d9cd646c
Makhsud Aminov <112966774+makhsudjonaminov@users.noreply.github.com>
2024-04-18 17:20:47 +0000

    Add files via upload

--- Routes/HomePage.jsx
+++ Routes/HomePage.jsx
@@ -1,12 +1,16 @@
 // File: routes/HomePage.js
 
 import React from 'react';
+import BgGif from "./../src/assets/bg-gif.gif"
 
 const HomePage = () => {
     return (
         <section className='Home'>
             <h1>Welcome to the Home Page</h1>
             <p>This is a simple heading and paragraph for the home page.</p>
+            <div className="gif">
+                <img src={BgGif} alt="" />
+            </div>
         </section>
     );
 }
