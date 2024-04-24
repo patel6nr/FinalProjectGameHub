170c22d4055f4e74992a2cf65a46a9cdca30e22f
patel6nr <patel6nr@mail.uc.edu>
2024-04-24 16:38:55 -0400

    Final

--- Components/Modal.jsx
+++ Components/Modal.jsx
@@ -2,6 +2,11 @@
 import React from 'react'
 
 export default function Modal({ isCorrect, solution, turn }) {
+    const handleReset = () => {
+        // Reload the browser window to perform a hard reset
+        window.location.reload();
+    };
+
     return (
         <div className="modal">
             {isCorrect && (
@@ -9,13 +14,15 @@ export default function Modal({ isCorrect, solution, turn }) {
                     <h1>You Win!</h1>
                     <p className="solution">{solution}</p>
                     <p>You found the word in {turn} guesses</p>
+                    <button onClick={handleReset}>Reset</button>
                 </div>
             )}
             {!isCorrect && (
                 <div>
                     <h1>Unlucky!</h1>
                     <p className="solution">{solution}</p>
-                    <p>Better luck next time</p>
+                    <p>Better luck next time</p>    
+                    <button onClick={handleReset}>Reset</button>
                 </div>
             )}
         </div>
